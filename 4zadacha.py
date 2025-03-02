3e2d21f076f477a9184ce00ea16b0a9c0c94c0c7
Aska Belenova <Belenova412@gmail.com>
2025-03-02 22:55:27 +0300

    3 лаба

---
+++ 4zadacha.py
@@ -0,0 +1,9 @@
+num1 = int(input("Введите значение 1 числа : "))
+num2 = int(input("Введите значение 2 числа : "))
+num3 = int(input("Введите значение 3 числа : "))
+
+average = (num1 + num2 + num3) / 3
+
+print("Среднее арифметическое:", average)
+
+print(" ")
---
+++ 5zadacha.py
@@ -0,0 +1,14 @@
+
+result1 = 5 + 2 * 3 - 4 / 2
+print("Результат 1:", result1)
+
+result2 = (3 + 5) * (2 + 4) / 2
+print("Результат 2:", result2)
+
+result3 = -3 + 6 / 2 * 4
+print("Результат 3:", result3)
+
+result4 = 5 + 4 * 5 ** 2 + 7
+print("Результат 4:", result4)
+
+print(" ")
